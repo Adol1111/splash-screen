f4889cebc0527b23ef5a14398c52f676b27586cd
gaoxiaoyi <gaoxiaoyi@hellobike.com>
2017-05-08 12:23:52 +0800

    添加可选parent节点

--- src/splash.ts
+++ src/splash.ts
@@ -8,18 +8,21 @@
  **/
 
 export interface SplashStatic {
-    enable(theme: string): void;
-    isRunning(): boolean;
-    destroy(): void;
-    version: string
+    enable(theme: string, element?: any): void;
+    isRunning(element?: any): boolean;
+    destroy(element?: any): void;
+    version: string;
 }
 
 let Splash: SplashStatic = {
     version: '2.4.0',
-    enable: function(theme) {
-        loadBody(function($body) {
+    enable: function (theme, element) {
+        loadBody(element, function ($body) {
+            if (Splash.isRunning($body)) {
+                Splash.destroy($body);
+            }
             addClass($body, 'splashing');
-            var $splash = splashDiv();
+            const $splash = splashDiv();
             $body.appendChild($splash);
 
             if (!theme || !themes[theme]) {
@@ -30,17 +33,18 @@ let Splash: SplashStatic = {
         });
     },
 
-    isRunning: function() {
+    isRunning: function (element?: any) {
+        if (element) return hasClass(element, 'splashing');
         if (!document || !document.body) {
             return;
         }
         return hasClass(document.body, 'splashing');
     },
 
-    destroy: function() {
-        loadBody(function($body) {
+    destroy: function (element?: any) {
+        loadBody(element, function ($body) {
             removeClass($body, 'splashing');
-            var $splash = getSplash($body);
+            const $splash = getSplash($body);
             if ($splash) {
                 $body.removeChild($splash);
             }
@@ -48,58 +52,58 @@ let Splash: SplashStatic = {
     }
 };
 
-var elementClass = function(tag, className) {
-    var ele = document.createElement(tag);
+const elementClass = function (tag, className) {
+    const ele = document.createElement(tag);
     ele.setAttribute('class', className);
     return ele;
 };
 
-var elementTxt = function(tag, text) {
-    var ele = document.createElement(tag);
+const elementTxt = function (tag, text) {
+    const ele = document.createElement(tag);
     ele.innerText = text;
     return ele;
 };
 
-var splashDiv = function() {
+const splashDiv = function () {
     return elementClass('div', 'splash');
 };
 
-var tailingHandler = function($splash) {
+const tailingHandler = function ($splash) {
     $splash.appendChild(elementTxt('span', 'Loading'));
 };
 
-var windcatcherHandler = function($splash) {
-    for (var i = 0; i < 8; i++) {
+const windcatcherHandler = function ($splash) {
+    for (let i = 0; i < 8; i++) {
         $splash.appendChild(elementClass('div', 'blade'));
     }
 };
 
-var circularHandler = function($splash) {
+const circularHandler = function ($splash) {
 
-    var arr = [
+    const arr = [
         'spinner-blue',
         'spinner-red',
         'spinner-yellow',
         'spinner-green'
     ];
 
-    for (var i = 0; i < arr.length; i++) {
-        var layer = elementClass('div', 'spinner-layer ' + arr[i]);
+    for (let i = 0; i < arr.length; i++) {
+        const layer = elementClass('div', 'spinner-layer ' + arr[i]);
 
-        var circleLeft = elementClass('div', 'circle-clipper left');
-        var circle01 = elementClass('div', 'circle');
+        const circleLeft = elementClass('div', 'circle-clipper left');
+        const circle01 = elementClass('div', 'circle');
 
         circleLeft.appendChild(circle01);
         layer.appendChild(circleLeft);
 
-        var gapPatch = elementClass('div', 'gap-patch');
-        var circle02 = elementClass('div', 'circle');
+        const gapPatch = elementClass('div', 'gap-patch');
+        const circle02 = elementClass('div', 'circle');
 
         gapPatch.appendChild(circle02);
         layer.appendChild(gapPatch);
 
-        var circleRight = elementClass('div', 'circle-clipper right');
-        var circle03 = elementClass('div', 'circle');
+        const circleRight = elementClass('div', 'circle-clipper right');
+        const circle03 = elementClass('div', 'circle');
         circleRight.appendChild(circle03);
 
         layer.appendChild(circleRight);
@@ -109,9 +113,10 @@ var circularHandler = function($splash) {
 
 };
 
-var emptyHandler = function() { };
+const emptyHandler = function () {
+};
 
-var themes = {
+const themes = {
     tailing: tailingHandler,
     windcatcher: windcatcherHandler,
     'audio-wave': emptyHandler,
@@ -120,42 +125,43 @@ var themes = {
     circular: circularHandler
 };
 
-var hasClass = function(ele, cls) {
+const hasClass = function (ele, cls) {
     return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
 };
 
-var addClass = function(ele, cls) {
+const addClass = function (ele, cls) {
     if (!hasClass(ele, cls)) {
         ele.className += ' ' + cls;
     }
 };
 
-var removeClass = function(ele, cls) {
+const removeClass = function (ele, cls) {
     if (hasClass(ele, cls)) {
-        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
         ele.className = ele.className.replace(reg, ' ');
     }
 };
 
-var loadBody = function(callback) {
-    var $body = document.body;
+const loadBody = function (ele, callback) {
+    if (ele)return callback(ele);
+    let $body = document.body;
     if ($body) {
         callback($body);
         return;
     }
-    setTimeout(function() {
+    setTimeout(function () {
         $body = document.body;
         if (!$body) {
-            loadBody(callback);
+            loadBody(ele, callback);
             return;
         }
         callback($body);
     }, 100);
 };
 
-var getSplash = function($body) {
-    var children = $body.children;
-    for (var i = 0; i < children.length; i++) {
+const getSplash = function ($body) {
+    const children = $body.children;
+    for (let i = 0; i < children.length; i++) {
         if (hasClass(children[i], 'splash')) {
             return children[i];
         }
